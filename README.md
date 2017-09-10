5c6e2fe07d6b6911e9764a86a138633dbb297d73
o.lauby <octave.lauby@beebryte.com>
2017-09-10 05:53:21 +0100

    First commit: readme and clean script

---
+++ README.md
@@ -0,0 +1,9 @@
+Bash scripts
+---
+
+
+# Scripts breakdown
+
+| script | description | has options |
+| --- | --- | --- |
+| [clean.sh](https://github.com/OctaveLauby/BashScripts/blob/master/clean.sh) | clean repository | true |
---
+++ clean.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+#title           : clean.sh
+#description     : This script clean a repository
+#==============================================================================
+
+
+CACHE=
+LOGS=
+TMP=
+
+
+display_help() {
+    echo "Usage: $0 [-a/-c/-l/-t] -- Delete files from wanted category."
+    echo
+    echo "-a, --all     clean all"
+    echo "-c, --cache   clean cache"
+    echo "-l, --logs    clean logs"
+    echo "-t, --tmp     clean temporary files"
+    echo
+    exit 1
+}
+
+no_option() {
+    echo "$0 is expecting at least one option."
+    display_help
+}
+
+
+HAS_OPT=
+while getopts haclt option
+do
+    case "${option}" in
+        a | all)
+            HAS_OPT="TRUE"
+            CACHE="TRUE"
+            LOGS="TRUE"
+            TMP="TRUE"
+            ;;
+        c | cache)
+            HAS_OPT="TRUE"
+            CACHE="TRUE"
+            ;;
+        l | logs)
+            HAS_OPT="TRUE"
+            LOGS="TRUE"
+            ;;
+        t | tmp)
+            HAS_OPT="TRUE"
+            TMP="TRUE"
+            ;;
+        h | \?)
+            HAS_OPT="TRUE"
+            display_help
+            ;;
+    esac
+done
+[[ -z  ${HAS_OPT} ]] && no_option
+
+
+[[ ! -z ${CACHE} ]] && rm -rf .cache
+[[ ! -z ${TMP} ]] && rm -rf _tmp
+[[ ! -z ${LOGS} ]] && rm -rf _logs
